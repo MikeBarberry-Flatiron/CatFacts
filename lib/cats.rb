ffd23977c2236e0fa4d0d04498aac5c3bc3fb992
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-03-28 16:27:04 -0700

    change api and add methods

--- lib/cats.rb
+++ lib/cats.rb
@@ -5,8 +5,10 @@ class Cats
     @@data = {}
 
     def initialize()
-        c = Curl::Easy.perform("https://cat-fact.herokuapp.com/facts")
-        d = c.body_str 
+        c = Curl::Easy.perform("https://api.thecatapi.com/v1/breeds") do |curl|
+            curl.headers['x-api-key'] = "7ee9d6b7-120d-475e-899d-7c5cb2353ef9"
+        end 
+        d = c.body_str
         e = JSON.parse(d)
         @@data = e
     end 
@@ -15,9 +17,28 @@ class Cats
         puts @@data
     end 
 
-    def showFacts()
+    def showBreeds()
        @@data.each do |x|
-        puts x["text"]
+        puts x["name"]
        end 
+    end 
+
+    def showBreedInfo(breed)
+        @@data.each do |x|
+            if x["name"] === breed
+                puts "About " + breed + " cats: " + x["description"]
+                puts "Lifespan: " + x["life_span"]
+                puts "Origin: " + x["origin"]
+                puts "Temperament: " + x["temperament"]
+            end 
+        end 
+    end 
+
+    def showWikiLink(breed)
+        @@data.each do |x|
+            if x["name"] === breed
+                puts "Visit this link: " + x["wikipedia_url"]
+            end 
+        end 
     end 
 end 
